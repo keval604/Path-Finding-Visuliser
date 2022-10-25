4485dc5a37216c7ac2b182b6c4e8fe7a65de44ad
Meet Sakhareliya <meetsakhareliya4702@gmail.com>
2022-10-25 07:41:22 +0530

    adding file before implementing dijkstra

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React, {useState } from "react";
 import Grid from "./Component/Grid/Grid";
 import Header from "./Component/Header/Header";
-<<<<<<< HEAD
-import MapContainer from "./Component/Map/MapContainer";
-=======
 import Map from "./Component/Map/GoogleMap";
 // import Dijikstra from "./Component/Map/dijikstra";
 // import VisContext from "./Context/vis-context";
->>>>>>> 5338d418253a50b6c34e9628d8a8675ff801618a
 
 const rows=20;
-const cols=40;
+  const cols=40;
 
-let visArray = (new Array(rows)).fill().map(function(){ return new Array(cols).fill(0);});
-let weightArray=[];
-for (let i = 0; i < rows; i++) {
-    weightArray.push([]);
-    for (let j = 0; j < cols; j++) {
-      weightArray[i].push(1);
-    }
-}
+  let visArray = (new Array(rows)).fill().map(function(){ return new Array(cols).fill(0);});
+  let weightArray=[];
+  for (let i = 0; i < rows; i++) {
+      weightArray.push([]);
+      for (let j = 0; j < cols; j++) {
+        weightArray[i].push(1);
+      }
+  }
 
 const initialCor={
-  start:{x:3,y:5},
-  end:{x:11,y:22}
+  start:{x:2,y:2},
+  end:{x:11,y:12}
 }
 
 
@@ -56,7 +52,7 @@ const App=()=>{
 
   return(
     <>
-      {/* <VisitContext.Provider
+      <VisitContext.Provider
         value={{
           visited:vis,  //grid array
           setVisited:visitHandler,
@@ -69,7 +65,7 @@ const App=()=>{
       >
           
         <GridInfoContext.Provider value={{ cor: cor, noRows: { rows }, noCols: { cols } }}>
-          {/* <Header /> */}
+          <Header />
           <Map></Map>
           
           {/* <Grid noRows={rows} noCols={cols} /> */}
@@ -79,13 +75,7 @@ const App=()=>{
           {/* <MapContainer> </MapContainer> */}
           {/* <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=72.53963738679887%2C23.12889068334543%2C72.5426307320595%2C23.130469312503628&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=19/23.12968/72.54113">View Larger Map</a></small> */}
         </GridInfoContext.Provider>
-<<<<<<< HEAD
-      </VisitContext.Provider> */}
-      <MapContainer></MapContainer>
-      
-=======
       </VisitContext.Provider>
->>>>>>> 5338d418253a50b6c34e9628d8a8675ff801618a
     </>
   )
 
--- src/Component/Map/GoogleMap.js
+++ src/Component/Map/GoogleMap.js
@@ -1,59 +1,55 @@
 import './map.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useMapEvents } from 'react-leaflet/hooks'
+import { useState } from 'react';
 
-const qop = require("query-overpass");
-const startNode = "5233360151";
-const endNode = "5233389505";
+// const qop = require("query-overpass");
+const qop = require('@derhuerst/query-overpass');
+const start = "5233360151";
+const end = "5233389505";
 
-const getCoordinate = (nodeId) => {
-   return new Promise((resolve, reject) => {
-            qop(`node(${nodeId});out geom;`, (data, x) => {
-            let c =  x.features[0].geometry.coordinates;
-            console.log(x);
-            resolve(c);
-         });
-   });   
-   // const y = qop(`node(${nodeId});out geom;`, (data, x) => {
-   //          let c =  x.features[0].geometry.coordinates;
-   //    });
-   // console.log("hiii");
-   // return y;
+const getCoordinate = async (nodeId) => {
+   const y = qop(`node(${nodeId});out geom;`, (data, x) => {
+      let cord = x.features[0].geometry.coordinates;
+      console.log(cord);
+   });
+   console.log(y);
+   return y;
+   console.log(nodeId);
+}
+// ,[23.1296306,72.5439978]
+function EndNode(props) {
+   const [cordinate, setCordinate] = useState([props.lat, props.lng]);
+   const [isMove, setIsMove] = useState(false);
+   const map = useMapEvents({
+      click: (e) => {
+         if (isMove == true) {
+            setCordinate([e.latlng.lat, e.latlng.lng]);
+            setIsMove(false);
+         }
+         
+    }
+   })
+   // draggable={true} 
+   return <Marker position={cordinate} eventHandlers={{
+      click: (e) => {
+         setIsMove(true);
+         console.log('marker clicked', e.latlng);
+      },
+    }}></Marker>;
 }
 
-const markPoint =  async () => {
-   const cords = await getCoordinate(startNode);
-   console.log(cords)
-   return cords;
-}
-
-function MyComponent() {
-  const map = useMapEvents({
-     click: () => {
-      map.locate()
-    },
-     locationfound: (location) => {
-        console.log('location found:', location);
-        markPoint();
-    },
-  })
-  return null
-}
-
-
-const Map = async () => {
+const Map = () => {
    return (
-      <MapContainer className='leaflet-container' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer className='leaflet-container' center={[23.1295773, 72.5419998]} zoom={17} scrollWheelZoom={true}>
          <TileLayer
          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
          />
-         
-         <MyComponent />
+         <EndNode lat = "23.1295773" lng = "72.541998" />
+         <EndNode lat="23.1296306" lng="72.5439978"/>
       </MapContainer>
-
    ); 
 }
-
 
 export default Map;
